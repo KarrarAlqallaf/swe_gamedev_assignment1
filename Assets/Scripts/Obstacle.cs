e0f9b9c31b26c9f91661a4a08233f002ac9e417b
Karrar Alqallaf <108615006+KarrarAlqallaf@users.noreply.github.com>
2026-09-16 20:57:48 +0300

    day 2, should be finishsed

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float minSize = 0.5f;
+    public float maxSize = 2.0f;
+    public float minSpeed = 150f;
+    public float maxSpeed = 300f;
+
+    Rigidbody2D rb;
+    public GameObject bounceEffectPrefab;
+
+    void Start()
+    {
+
+        float randomSize = Random.Range(minSize, maxSize);
+        transform.localScale = new Vector3(randomSize, randomSize, 1);
+
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+        Vector2 randomDirection = Random.insideUnitCircle;
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(randomDirection * randomSpeed);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+    Vector2 contactPoint = collision.GetContact(0).point; 
+    GameObject bounceEffect = Instantiate(bounceEffectPrefab, contactPoint, Quaternion.identity);
+
+    // Destroy the effect after 1 second
+    Destroy(bounceEffect, 1f);
+    }
+}
+
+
